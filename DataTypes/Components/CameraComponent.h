8ac94b17bcd511d32b7ef8935ec2831276cc90ab
Abror <Krubka_Jo@protonmail.com>
2026-03-04 11:08:37 +0500

    Some UI and UX fixes

--- DataTypes/Components/CameraComponent.h
+++ DataTypes/Components/CameraComponent.h
@@ -43,14 +43,13 @@ public:
     void DrawComponentUI(AssetManager& AssetManager){
         ImGui::PushID(ID);
         ImGui::Text("Zoom");
-        ImGui::SameLine();
         ImGui::DragFloat("##x", &Zoom, 0.1f, 0.01f, 20.0f);
         ImGui::Text("Near");
-        ImGui::SameLine();
         ImGui::DragFloat("##xx", &near, 0.1f, -10.0f, 0.0f);
          ImGui::Text("Far");
-        ImGui::SameLine();
         ImGui::DragFloat("###xx", &far, 0.1f, 0.0f, 10.0f);
+        ImGui::Dummy(ImVec2(0, 5));
+        ImGui::Separator();
         ImGui::PopID();
     };
 };
