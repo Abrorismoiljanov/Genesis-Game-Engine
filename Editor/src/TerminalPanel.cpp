4f72ac56ca52b6ae94bcd1a33f669bfca9ebb9d1
Abror <Krubka_Jo@protonmail.com>
2026-03-20 12:41:27 +0500

    Made Colored Types

--- Editor/src/TerminalPanel.cpp
+++ Editor/src/TerminalPanel.cpp
@@ -23,16 +23,22 @@ void Terminal::Render(){
     for (auto& l : logs) {
 
         ImVec4 color;
-
+        ImVec4 colorType;
         switch (l.level) {
             case LogLevel::Info:    color = ImVec4(1,1,1,1); break;
             case LogLevel::Warning: color = ImVec4(1,1,0,1); break;
             case LogLevel::Error:   color = ImVec4(1,0.3f,0.3f,1); break;
         }
+        switch (l.system) {
+            case LogSystem::Engine : colorType = ImVec4(1,1,0,1); break;
+            case LogSystem::Renderer : colorType = ImVec4(0,1,1,1); break;
+            case LogSystem::Script : colorType = ImVec4(0,1,0,1); break;
+        }
 
-        ImGui::TextColored(color, "[%s] %s",
-            Log->LogSystemToString(l.system),
-            l.message.c_str());
+        ImGui::TextColored(colorType, "[%s]",Log->LogSystemToString(l.system));
+        ImGui::SameLine();
+        ImGui::TextColored(color, "%s", l.message.c_str());
+
     }
 
     if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
--- assets/scripts/TestGame/Player.lua
+++ assets/scripts/TestGame/Player.lua
@@ -9,15 +9,19 @@ function script:OnUpdate(entity, dt)
     local speed = 100.0;
 
     if Input.IsKeyDown(KEY_W) then
+        Log("W is pressed")
         entity.transform.position.y = entity.transform.position.y + speed * dt
     end
     if Input.IsKeyDown(KEY_S) then
+        Log("S is pressed")
         entity.transform.position.y = entity.transform.position.y - speed * dt
     end
     if Input.IsKeyDown(KEY_A) then
+        Log("A is pressed")
         entity.transform.position.x = entity.transform.position.x - speed * dt
     end
     if Input.IsKeyDown(KEY_D) then
+        Log("D is pressed")
         entity.transform.position.x = entity.transform.position.x + speed * dt
     end
 end
